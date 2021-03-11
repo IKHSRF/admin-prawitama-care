0557ecd29be0c436dbc0447502d5657589ea74df
Ikhsan Arfian Nugraha <ikhsan3568@gmail.com>
2021-03-11 12:41:03 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/prawitama_care_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.prawitama_care_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:prawitama_care_admin/common/style.dart';
+import 'package:url_strategy/url_strategy.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: textTheme,
+      ),
+    );
+  }
+}
